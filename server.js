e9dfd1f984fbae89332dab106732049d05d5725e
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-10-31 11:36:41 +0530

    🚀 created new db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config; 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://whitelegend56:xYF6exMBZqXQLDfI@website.ddp2glq.mongodb.net/dynamic-chat-app");
+mongoose.connect("mongodb+srv://whitelegend56:S89mNrZmoYWdrLYa@webpage.hlfcfsn.mongodb.net/dynamic-chat-app");
 const app = require('express')();
 const http = require('http').Server(app);
 const useroute = require("./routes/UserRoutes");
